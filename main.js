d5e276ef80dc09f5a318c94e2ec5639e88177cae
Cova14 <covaslender@gmail.com>
2020-02-13 19:07:25 -0600

    Adding todos on list

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-import * as Redux from 'redux'
+// import * as Redux from 'redux'
+// nodes
+let input = document.getElementById("input")
+let list = document.getElementById("list")
+let todos = {}
+//function
+function drawTodos(){
+  list.innerHTML = ""
+  for(let key in todos) {
+    let li = document.createElement('li')
+    li.id = key
+    li.innerHTML = `
+      <span>${todos[key]}</span>
+      <span id="${key}">X</span>
+    `
+    list.appendChild(li)
+  }
+}
 
-console.log("Estoy bien instalado")
-console.log(Redux)
+//Listeners
+input.addEventListener('keydown', e => {
+  if(e.key === "Enter") {
+    let text = e.target.value
+    let id = Object.keys(todos).length
+    todos[id] = text
+    drawTodos()
+  }
+})
