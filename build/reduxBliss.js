f15571896afab1ace1076973dfff30056a71ec00
Cova14 <covaslender@gmail.com>
2020-02-13 19:41:06 -0600

    Changing class on span click

--- build/reduxBliss.js
+++ build/reduxBliss.js
@@ -90,32 +90,58 @@
 
 // import * as Redux from 'redux'
 // nodes
-let input = document.getElementById("input")
-let list = document.getElementById("list")
-let todos = {}
+let input = document.getElementById("input");
+let list = document.getElementById("list");
+let todos = {
+  0: {
+    text: "Ir al cine",
+    done: false
+  },
+  1: {
+    text: "Cenar",
+    done: true
+  },
+  2: {
+    text: "Grabar",
+    done: false
+  }
+};
 //function
 function drawTodos(){
-  list.innerHTML = ""
+  list.innerHTML = "";
   for(let key in todos) {
-    let li = document.createElement('li')
-    li.id = key
+    let li = document.createElement('li');
+    // li.id = key;
+    let classDone = todos[key].done ? "done" : ""
     li.innerHTML = `
-      <span>${todos[key]}</span>
-      <span id="${key}">X</span>
-    `
-    list.appendChild(li)
+      <span id="${key}" class="${classDone}">${todos[key].text}</span>
+      <span>X</span>
+    `;
+    setListeners(li);
+    list.appendChild(li);
   }
-}
+};
+
+function setListeners(li){
+  li.addEventListener('click', e => {
+    console.log(e.target)
+    let key = e.target.id;
+    todos[key].done = !todos[key].done
+    drawTodos()
+  });
+};
 
 //Listeners
 input.addEventListener('keydown', e => {
   if(e.key === "Enter") {
-    let text = e.target.value
-    let id = Object.keys(todos).length
-    todos[id] = text
-    drawTodos()
+    let text = e.target.value;
+    let id = Object.keys(todos).length;
+    todos[id] = {text, done:false};
+    drawTodos();
   }
-})
+});
+
+drawTodos()
 
 /***/ })
 /******/ ]);
